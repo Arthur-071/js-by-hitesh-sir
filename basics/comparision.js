e23a3e33fa2ea84c90fb3481709c75c1f9fbbfa2
Arthur-071 <arthur7nova@gmail.com>
2025-10-27 12:12:19 +0000

    comparision

---
+++ basics/comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); datatype conversion takes place
+// console.log("02" > 1);
+
+console.log(null > 0); //false
+console.log(null == 0); // datatype conversion does not takes place (false)
+console.log(null >= 0); //true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2); //no conversion allowed
