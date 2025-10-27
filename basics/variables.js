cb3a9a3d7f326866677589b9e1b3ffdcf5574bc8
Arthur-071 <arthur7nova@gmail.com>
2025-10-27 11:30:26 +0000

    variables and datatypes

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 23554 // cant change at any point of time
+let accountEmail= "ghd@Me.com"
+var accountPassword = "54685" // not used in the industry
+accountCity = "jaipur" // this method of variable declaration is not recommended
+// tc39 doc and mdn doc are important
+let accountState
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
